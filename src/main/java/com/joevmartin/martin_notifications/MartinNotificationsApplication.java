ecf61a27db6869636df5478c7069584cff319f90
Joe Martin <martin@360works.com>
2024-11-27 16:22:46 -0500

    setting up spring project

---
+++ src/main/java/com/joevmartin/martin_notifications/MartinNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.joevmartin.martin_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartinNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartinNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joevmartin/martin_notifications/MartinNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joevmartin.martin_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartinNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
